2c747a9500ab7ba07d9e6fcee694d068e376c711
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2023-04-18 15:07:29 +0100

    make userpage responsive on mobile

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import './style.css';
 import loadHome from './modules/homepage';
 import loadUserPage from './modules/userpage';
 
-// loadHome();
-loadUserPage()
-
-title.innerText = 'Test';
+loadHome();
+// loadUserPage()
